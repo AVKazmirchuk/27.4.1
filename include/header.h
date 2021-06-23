db9398e4d2cb8e469db7471d2c5fa5cebab93dae
AVKazmirchuk <85755971+AVKazmirchuk@users.noreply.github.com>
2021-06-23 22:03:25 +0300

    Add files via upload

---
+++ include/header.h
@@ -0,0 +1,80 @@
+#pragma once
+
+class Home
+{
+private:
+	std::string AlfName{};
+public:
+	void setName(std::string& name);
+
+	std::string& getName();
+};
+
+//Средняя ветвь
+
+class Normal
+{
+private:
+	Home* home;
+public:
+	Normal();
+
+	~Normal();
+
+	Home* getHome();
+};
+
+//Большая ветвь
+
+class Big
+{
+private:
+	const int numberOfNormals;
+	Normal** normals;
+	Home* home;
+public:
+	Big(int const inNumberOfNormals);
+
+	~Big();
+
+	int getNumberOfNormals();
+
+	Normal* getNormalByIndex(int idx);
+
+	Home* getHome();
+
+	int numberOfNeighbors();
+};
+
+class Tree
+{
+private:
+	const int numberOfBigs;
+	Big** bigs;
+public:
+	Tree(const int inNumberOfBigs, const int normalMin, const int normalMax);
+
+	~Tree();
+
+	int getNumberOfBigs();
+
+	Big* getBigByIndex(int idx);
+};
+
+class Forest
+{
+private:
+	const int numberOfTrees;
+	Tree** trees;
+public:
+	Forest(const int InNumberOfTrees, const int bigsMin, const int bigsMax,
+		const int normalMin, const int normalMax);
+
+	~Forest();
+
+	int getNumberOfTrees();
+
+	Tree* getTreeByIndex(int idx);
+
+	Big* search();
+};
